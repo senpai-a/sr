dd909ac612e0a9c30a1b4d194530c29fd0f1a740
Biuuuuuuuu <14502754+senpai-a@users.noreply.github.com>
2019-04-20 18:13:01 +0800

    ploterr

--- ploterr.py
+++ ploterr.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import sys
+import numpy as np
 from matplotlib import pyplot as plt
 
 err1 = 'results/filter_91_err/classError.p'
@@ -19,5 +20,15 @@ with open(len2,'rb') as f:
 mse1 = err1.astype('float')/len1
 mse2 = err2.astype('float')/len2
 dmse = mse2 - mse1
-plt.bar(range(len(dmse)),dmse)
-plt.show
+print(dmse)
+i = 0
+fig,axs=plt.subplots(3,4,sharex=True, sharey=True)
+for co in range(3):
+    for pt in range(4):
+        i+=1
+        ax=axs[co,pt]
+        plot1=ax.matshow(dmse[:,:,co,pt])
+
+fig.colorbar(plot1,ax=[axs[:,:]],location='right',shrink = 0.5)
+plt.tight_layout()
+plt.show()
